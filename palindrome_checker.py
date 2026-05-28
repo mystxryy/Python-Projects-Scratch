22cc0191b9bcfec4034e0ee55cc4b90bbf12d76d
Sanidhya Shukla <sanidhya.mystery@gmail.com>
2026-05-28 08:38:20 +0530

    Update palindrome_checker.py

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,3 +1,6 @@
+print("=" * 50)
+print("      PALINDROME FINDER")
+print("=" * 50)
 str1=input("Enter a string for palindrome check: ")
 if str1==str1[::-1]:
     print(f"The string {str1} is a palindrome.")
