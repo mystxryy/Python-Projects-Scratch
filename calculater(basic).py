c781d82bd985a0e14f396d26fee775938944c1f3
Sanidhya Shukla <sanidhya.mystery@gmail.com>
2026-05-28 08:38:43 +0530

    Update calculater(basic).py

--- calculater(basic).py
+++ calculater(basic).py
@@ -1,3 +1,6 @@
+print("=" * 50)
+print("      BASIC CALCULATER")
+print("=" * 50)
 num1=int(input("Enter the first number: "))
 num2=int(input("Enter the second number: "))
 print("What do u want to do with these numbers?")
